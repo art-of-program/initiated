083af78e5a6ae96b6acdfeb29566e89e2773df6b
crack-dev <abdulrahmonu@gmail.com>
2026-01-08 06:28:36 -0500

    rust: variables & mutability

--- common-programming-concepts/src/main.rs
+++ common-programming-concepts/src/main.rs
@@ -1,3 +1,15 @@
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    // variables & mutability
+    let mut i = 5; // mutable variable
+    println!("\nThe value of i is: {i}");
+    i = 6;
+    println!("The value of i is: {i}");
+
+    let x = 10; // immutable variable (by default)
+    println!("The value of x is: {x}")
 }
